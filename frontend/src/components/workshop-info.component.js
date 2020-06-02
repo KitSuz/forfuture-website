53de0916f0828eaa04c032219c7648e700b0e7ff
KitSuz <65182249+KitSuz@users.noreply.github.com>
2020-06-02 12:22:08 +0200

    Update workshop-info.component.js

--- frontend/src/components/workshop-info.component.js
+++ frontend/src/components/workshop-info.component.js
@@ -53,7 +53,7 @@ function WorkshopInfo({backLink}) {
            firstname, lastname, email, age, message
        }
        SignupService.Signup(data).then((data) => {
-           alert("Deine Anmeldung wurde erfolgreich abgeschickt. Du erhälst in Kürze Rückmeldung von uns!");
+           alert("Deine Anmeldung wurde erfolgreich abgeschickt. Du erhälst in spätestens 48h eine Rückmeldung von uns!");
        }).catch((error) => {
            alert("Fehler: " + error.message);
        })
@@ -66,17 +66,57 @@ function WorkshopInfo({backLink}) {
                 <div>
                     <TypeAnimationComponent />
 
+                    <h2><span className="underline">Du</span> kannst etwas bewegen. Sei es auf der Straße, in der Politik oder an deinem Schreibtisch.</h2>
+        
                     <a onClick={() => { 
                         setY({ y: scrollDestinationRef.current.getBoundingClientRect().top - 20, from: { y: document.querySelector(".page").scrollTop }, });
-                        }} className="button signup-button">Anmelden</a>
+                        }} className="button signup-button">Beim Projekt anmelden</a>
+                    <p>Für jede Anmeldung wird <span className="underline">ein Baum gepflanzt</span>.</p>
 
-                    <h2>»_forfuture» sind strategische <span className="underline">Workshops</span> zum Experimentieren, Lernen & Begreifen, wie wir schon heute <span className="underline">unsere Zukunft gestalten</span> können!</h2>
+                    <h2>Wir suchen <span className="underline">dich</span> für einen kreativen workshop.</h2>
+                    <p>Entdecke deine Stärken.Lerne professionelle Methoden, um nachhaltige Lösungen zu finden. Arbeite in einem engagierten und kreativen Team. Hilf mit, an einer idealen Zukunft zu arbeiten </p>
+                    <p>
+                    Kostenlos anmelden und an einem 2-tägigen Intensiv-Workshop zum Thema »Nachhaltigkeit» teilnehmen.<br />
+                    </p>          
+                        
+                    <h2>Warum kostenlos Teilnehmen?</h2>
+                    <p>Dieser Workshop ist praktischer Teil einer Bachelorarbeit im Bereich Design. Deswegen fallen weder Kosten noch Werbung an. </p>
 
-                    <h3>Über mich</h3>
-                    <p>Ich bin Katrin Schulz, Designstudentin im neunten Semester an der HAW Hamburg (Hochschule für Angewandte Wissenschaften), 24 Jahre alt und liebe Sport, Musik und Kochen. Ich gehe gerne raus und mache mir viele Gedanken, wie die Welt wohl so in 20, 30 Jahren aussehen wird…</p>
-                    <img alt="profile picture" src={Profile}/>
+                    
+                    <h3 ref={scrollDestinationRef}>Anmelden</h3>
+                    <h2>Es sind nur <span className="underline">7 Plätze</span> verfügbar!</h2>
+                    <p>
+                    Melde dich bis zum 8.6.2020 an und bekommst 48h später Bescheid, ob du beim Workshop dabei bist. <br />
+                    <br />
+                    Bei Fragen vorab, schreib uns einfach eine Mail an: info@forfuture.studio <br />
+                    </p>
 
-                    <h3>Das Projekt</h3>
+                    <input type="text" value={firstname} onChange={onChangeFirstname} placeholder="Vorname"></input>
+                    <input type="text" value={lastname} onChange={onChangeLastname} placeholder="Nachname"></input>
+                    <input type="email" value={email} onChange={onChangeEmail} placeholder="E-Mail Adresse"></input>
+                    <div className="age-select">
+                        <span>Alter:</span>
+                        <SelectorComponent value={items.indexOf(age)} onChange={onChangeAge} items={items}></SelectorComponent>
+                    </div>
+                    <textarea value={message} onChange={onChangeMessage} placeholder="Nachricht (max 100 Zeichen)" maxLength="100" rows="4"></textarea>
+                    <button onClick={SignupClicked} style={{marginTop: "1.4em"}}>Anmeldung abschicken</button>
+                    
+                    <h3>Workshop Termine</h3>
+                    <h2>13.06.2020 (Samstag) ab 10 uhr - open end</h2>
+                    <p>
+                    Kennenlernen, Problem besprechen, Recherchieren, Personas erstellen, Standpunkt definieren<br />
+                    </p>    
+                    <h2>13.06.2020 (Samstag) ab 10 uhr - open end</h2>   
+                    <p>
+                   Ideen entwickeln, Prototypen bauen, Testen und Feedback einholen, Abschluss-Reflektion<br />
+                    </p>
+                        
+                        
+                        
+                        
+                    <h2>Mehr Informationen:</h2>
+
+                    <h3>Über den Workshop</h3>
                     <p>
                     Das Projekt »_forfuture» ist der praktische Teil meiner Bachelorarbeit, in der ich mich mit Social Design beschäftige. <br />
                     <br />
@@ -93,7 +133,7 @@ function WorkshopInfo({backLink}) {
                     <span className="underline">Wie können wir da besser werden und vielleicht eine Antwort finden</span>? Möglicherweise eine Antwort, die am Ende nich tnur uns, sondern mehr Menschen hilft, denen es ähnlich geht. <br />    
                     </p>
 
-                    <h3>Unsere Zusammenarbeit</h3>
+                    <h3>Als Team arbeiten</h3>
                     <p>
                         Das Team zu einer Zukunfts-Verändernden Frage wie dieser (siehe oben), muss agil sein, unkonventionell (also »andersdenkend»), engagiert und nicht erwachsen! Unsere Gruppe wird nicht größer sein als <span className="underline">acht Personen</span> (mich eingeschlossen). Hinterher machen die vielseitigen Blickwinkel ein solches Projekt stark. Das heißt Nachhaltigkeits-Profis sowie »Just-Starter» sind herzlich willkommen! <br />
                         <br />
@@ -103,37 +143,24 @@ function WorkshopInfo({backLink}) {
                         <br />
                     </p>
 
-                    <h3>Durchführung während Corona</h3>
+                    <h3>Korona-Konform</h3>
                     <p>
                         Das ideale Umfeld solcher Art Workshop ist normalerweise ein großer Tisch und vielen Materialen, wie Zettel, Stift, Klebe, vielleicht Holz, Knete, etc. In Zeiten von Korona müssen wir ein wenig umdisponieren und werden uns größtenteils auf digitalem Wege treffen – über das Internet. Dafür ist es wichtig, dass du einen Computer oder Laptop mit Internetzugang hast. Auch ein Mikro und eine Kamera wären gut, damit wir uns hören und sehen können. Zumindest Laptops haben Mikros und Kameras eingebaut. 
                     </p>
 
-                    <h3>Workshop-Termine</h3>
-                    <p>
-                        Mo, 08.06. bis Fr, 12.06.2020 (inklusive),<br />
-                        immer 14 bis 18 Uhr <br />
-                        Workshops: »Verstehen & Beobachten», »Standpunkt finden», »Ideen entwickeln», »Prototyp kreieren», »Testen» und »Feedback»<br />
-                    </p>
-
-                    <h3 ref={scrollDestinationRef}>Anmelden</h3>
-                    <p>
-                        Du möchtest bei dem Projekt dabei sein? Dann trag dich gern ein!
-                        Bei Fragen, schreib mir einfach ‘ne Mail an: info@forfuture.studio
-                    </p>
-                    <input type="text" value={firstname} onChange={onChangeFirstname} placeholder="Vorname"></input>
-                    <input type="text" value={lastname} onChange={onChangeLastname} placeholder="Nachname"></input>
-                    <input type="email" value={email} onChange={onChangeEmail} placeholder="E-Mail Adresse"></input>
-                    <div className="age-select">
-                        <span>Alter:</span>
-                        <SelectorComponent value={items.indexOf(age)} onChange={onChangeAge} items={items}></SelectorComponent>
-                    </div>
-                    <textarea value={message} onChange={onChangeMessage} placeholder="Nachricht (max 100 Zeichen)" maxLength="100" rows="4"></textarea>
-                    <button onClick={SignupClicked} style={{marginTop: "1.4em"}}>Anmeldung abschicken</button>
-                    <p>Bis spätestens <span className="underline">03.06.2020</span> bekommst du Bescheid, ob du bei dem Projekt dabei bist. Ich freu mich auf dich!</p>
-
-                    <h3>Du bist jünger als 18?</h3>
+                    <h3>Minderjährig?</h3>
                     <p>Sprich mit deinen Eltern! Zeig ihnen diese Seite und hole dir ihr »okay», an diesem Projekt teilzunehmen. Nach der Anmeldung bekommst du in der Bestätigungsmail ein Einverständnis-Formular zugeschickt, das deine Eltern ausfüllen und zurückschicken müssen. Andernfalls darf ich leider (rechtlich) nicht mit dir arbeiten und das wäre schade! </p>
                 
+                    <h3>Über uns</h3>
+                    <p>Ich bin Katrin Schulz, Designstudentin im neunten Semester an der HAW Hamburg (Hochschule für Angewandte Wissenschaften), 24 Jahre alt und liebe Sport, Musik und Kochen. Ich gehe gerne raus und mache mir viele Gedanken, wie die Welt wohl so in 20, 30 Jahren aussehen wird…</p>
+                    <img alt="profile picture" src={Profile}/>    
+                    
+                    <img alt="HAW Hamburg Logo" src={HAW} />
+
+                    <p>
+                        Eine Bachelorarbeit der Hochschule für Angewandte Wissenschaften Hamburg, im Bereich Kommunikationsdesign, <span className="underline">Referenz-Kontakt</span>: Hendrike Schmietendorf, Öffentlichkeitsarbeit Fakultät DMI, Mail: Hendrike.Schmietendorf@haw-hamburg.de
+                    </p>
+                    
                     <h3 id="policy">Datenschutz</h3>
                     <p>Mit deinen Daten soll gut umgegangen werden! Aus dem Grund sind auf dieser Seite keine Analytics- oder Trackingfunktionen eingebaut und lediglich <span className="underline">seriöse</span> Drittanbieter eingebunden, die den <span className="underline">Richtlinien der DSGVO</span> folgen. Das heißt, sie haben ihre Server in Deutschland, sodass keine Datenweitergabe stattfindet! Möchtest du mehr erfahren? Dann lies in den Bestimmungen die Details nach:</p>
                     <Link className="button" to={{
@@ -150,12 +177,6 @@ function WorkshopInfo({backLink}) {
                             backLink: backLink
                         }
                     }}>Impressum</Link>
-
-                    <img alt="HAW Hamburg Logo" src={HAW} />
-
-                    <p>
-                        Eine Bachelorarbeit der Hochschule für Angewandte Wissenschaften Hamburg, im Bereich Kommunikationsdesign, <span className="underline">Kontakt</span>: Hendrike Schmietendorf, Öffentlichkeitsarbeit Fakultät DMI, Mail: Hendrike.Schmietendorf@haw-hamburg.de
-                    </p>
 
                     <p><br/>© Katrin Schulz, 2020</p>
 
